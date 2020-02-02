ca70074400f55504f5ebd3148cb89506dbe44d23
nickfrostus <nickfrost.us@gmail.com>
2020-02-02 11:23:13 -0500

    table refinements

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,25 +4,27 @@
 
 @section('content')
     @if($users->count())
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Name</th>
-                <th>E-mail</th>
-                <th>Created</th>
-                <th>Last Updated</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($users as $user)
+        <div class="table-container">
+            <table class="table is-striped is-hoverable">
+                <thead>
                 <tr>
-                    <td>{{ $user->first_name }} {{ $user->last_name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->created_at }}</td>
-                    <td>{{ $user->updated_at }}</td>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                    <th>Created</th>
+                    <th>Last Updated</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->first_name }} {{ $user->last_name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->created_at }}</td>
+                        <td>{{ $user->updated_at }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     @endif
 @endsection
