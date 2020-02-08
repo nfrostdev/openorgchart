9527fe41a00f347a537242f74aa60b4db8ea4c57
nickfrostus <nickfrost.us@gmail.com>
2020-02-07 23:07:08 -0500

    moved some css, team component abstraction

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,81 @@
         h1, h2, p {
             font-family: 'Gelasio', serif !important;
         }
+
+        .org-chart-group {
+            display: grid;
+            align-items: start;
+            grid-gap: 0 1.5rem;
+            padding: 1.5rem 0.5rem 4rem;
+            overflow: hidden;
+            grid-template-columns: repeat(2, 1fr);
+        }
+
+        .org-chart-group .box {
+            margin: 0 0 1.5rem !important;
+        }
+
+        .org-chart-group-department {
+            grid-template-columns: 1fr;
+            max-width: 100vw;
+            overflow: auto;
+        }
+
+        .department-leader {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 125%;
+            margin-bottom: 3rem;
+        }
+
+        .org-chart-employee {
+            position: relative;
+            display: inline-flex !important;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .org-chart-employee.leader {
+            z-index: 10;
+            justify-self: center;
+            margin-bottom: 0 !important;
+        }
+
+        .org-chart-employee-pointer {
+            position: absolute;
+            display: inline-flex;
+            width: 0.813rem;
+            background-color: #3273dc;
+            height: 0.125rem;
+            pointer-events: none;
+        }
+
+        .org-chart-employee-pointer__top {
+            top: -150%;
+            height: 200%;
+            width: 0.125rem;
+            z-index: -1;
+        }
+
+        .org-chart-employee:nth-child(odd):not(.leader) {
+            justify-self: end;
+        }
+
+        .org-chart-employee:nth-child(even):not(.leader) {
+            justify-self: start;
+        }
+
+        .org-chart-employee:nth-child(odd) .org-chart-employee-pointer,
+        .org-chart-employee:nth-child(odd) .org-chart-employee-pointer__top {
+            right: -0.813rem;
+        }
+
+        .org-chart-employee:nth-child(even) .org-chart-employee-pointer,
+        .org-chart-employee:nth-child(even) .org-chart-employee-pointer__top {
+            left: -0.813rem;
+        }
     </style>
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
