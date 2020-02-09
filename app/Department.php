a8f217ee76502082b93e4942da01d71ed7f2e886
nickfrostus <nickfrost.us@gmail.com>
2020-02-09 17:40:12 -0500

    complex relation returns for departments and employees

--- app/Department.php
+++ app/Department.php
@@ -12,4 +12,24 @@ class Department extends Model
     {
         return $this->hasOne('App\Employee', 'id', 'employee_id');
     }
+
+    private function recursiveEmployeeCount($supervisor)
+    {
+        $employees = $supervisor->team;
+        if ($employees) {
+            // Check for nested teams.
+            foreach ($supervisor->team as $employee) {
+                $employees->push($this->recursiveEmployeeCount($employee));
+            }
+        }
+        $employees->push($supervisor);
+        // TODO: This is pretty sloppy. There has to be a better way.
+        return $employees->flatten()->unique();
+    }
+
+    // https://laravel.com/docs/master/eloquent-mutators#defining-an-accessor
+    public function getEmployeesAttribute()
+    {
+        return $this->recursiveEmployeeCount($this->leader);
+    }
 }
--- app/Employee.php
+++ app/Employee.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Department;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
@@ -16,5 +17,21 @@ class Employee extends Model
     public function team()
     {
         return $this->hasMany('App\Employee', 'supervisor_id', 'id');
+    }
+
+    // Checking recursively for supervisor assignments.
+    public function getRecursiveSupervisor($employee)
+    {
+        // If this employee has a supervisor, see if the supervisor has one too. Otherwise this should be the highest level of supervision.
+        return $employee->supervisor ? $this->getRecursiveSupervisor($employee->supervisor) : $employee;
+    }
+
+    // https://laravel.com/docs/master/eloquent-mutators#defining-an-accessor
+    public function getDepartmentAttribute()
+    {
+        // Get the highest level of supervision.
+        $employee = $this->getRecursiveSupervisor($this);
+        // Return the correlating department.
+        return Department::where('employee_id', $employee->id)->first();
     }
 }
