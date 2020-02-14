c07434b9ed79652d6905f4996bd176a15ff9c918
nickfrostus <nickfrost.us@gmail.com>
2020-02-13 22:36:20 -0500

    local anchor link to supervisor

--- resources/views/components/employee.blade.php
+++ resources/views/components/employee.blade.php
@@ -1,15 +1,19 @@
-<div class="employee  {{ $employee->team->count() > 0 ? 'supervisor' : '' }}">
+<div id="{{ $employee->id }}_{{ $employee->first_name }}_{{ $employee->last_name }}"
+     class="employee  {{ $employee->team->count() > 0 ? 'supervisor' : '' }}">
     <div class="box has-text-centered employee-box {{ isset($leader) && $leader ? 'leader' : '' }} {{ $employee->team->count() > 0 ? 'supervisor' : '' }}">
         @if($employee->supervisor)
-            <div class="employee-box-reports-to has-text-gray-light" title="Reports to {{ $employee->supervisor->first_name }} {{ $employee->supervisor->last_name }}">
+            <a href="#{{ $employee->supervisor->id }}_{{ $employee->supervisor->first_name }}_{{ $employee->supervisor->last_name }}"
+               class="employee-box-reports-to has-text-gray-light" title="Reports to {{ $employee->supervisor->first_name }} {{ $employee->supervisor->last_name }}">
                 <span class="fas fa-arrow-up"></span>
                 <span class="employee-box-reports-to-text">{{ $employee->supervisor->first_name }} {{ $employee->supervisor->last_name }}</span>
-            </div>
+            </a>
         @endif
-        <div class="has-text-weight-semibold {{ $employee->team->count() > 0 ? 'has-text-link' : '' }} employee-box-name">
-            {{ $employee->first_name }} {{ $employee->last_name }}
+        <div>
+            <div class="has-text-weight-semibold {{ $employee->team->count() > 0 ? 'has-text-link' : '' }} employee-box-name">
+                {{ $employee->first_name }} {{ $employee->last_name }}
+            </div>
+            <div class="employee-box-title is-size-7">{{ $employee->title }}</div>
         </div>
-        <div class="employee-box-title is-size-7">{{ $employee->title }}</div>
     </div>
 
     @if($employee->team->count() > 0)
