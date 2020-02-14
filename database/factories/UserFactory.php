09ba294ebb414a0a5f60e8730a7e64fd72e41b72
nickfrostus <nickfrost.us@gmail.com>
2020-02-13 22:15:30 -0500

    user factory role seeding

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,6 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-        // TODO: Roles
+        'role_id' => $faker->boolean ? \App\Role::all()->random()->id : null
     ];
 });
--- resources/views/components/employee.blade.php
+++ resources/views/components/employee.blade.php
@@ -1,6 +1,6 @@
 <div class="employee  {{ $employee->team->count() > 0 ? 'supervisor' : '' }}">
     <div class="box has-text-centered employee-box {{ isset($leader) && $leader ? 'leader' : '' }} {{ $employee->team->count() > 0 ? 'supervisor' : '' }}">
-        @if($employee->supervisor && $employee->team->count() > 0)
+        @if($employee->supervisor)
             <div class="employee-box-reports-to has-text-gray-light" title="Reports to {{ $employee->supervisor->first_name }} {{ $employee->supervisor->last_name }}">
                 <span class="fas fa-arrow-up"></span>
                 <span class="employee-box-reports-to-text">{{ $employee->supervisor->first_name }} {{ $employee->supervisor->last_name }}</span>
