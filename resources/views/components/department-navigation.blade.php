8139be692dfd74f1e631b32a31640bf97b7dd452
nickfrostus <nickfrost.us@gmail.com>
2020-02-17 19:37:14 -0500

    changes to department navigation

--- resources/views/components/department-navigation.blade.php
+++ resources/views/components/department-navigation.blade.php
@@ -1,8 +1,28 @@
 @if($departments->count() > 0)
-    <div class="content buttons is-centered">
-        @foreach($departments as $department)
-            <a href="{{ route('departments.show', ['department' => $department->id]) }}"
-               class="button card {{ isset(Route::current()->parameters()['department']) && Route::current()->parameters()['department']->id === $department->id ? 'is-active is-link' : '' }}">{{ $department->name }}</a>
-        @endforeach
+    <div class="content has-text-centered">
+        <label for="choose-department" class="sr-only">Department Selection</label>
+        <div class="select card">
+            <select id="choose-department" oninput="navigateToDepartmentSelection()">
+                <option value="">Select a Department</option>
+                @foreach($departments as $department)
+                    <option value="{{ route('departments.show', ['department' => $department->id]) }}"
+                        {{ isset(Route::current()->parameters()['department']) && Route::current()->parameters()['department']->id === $department->id ? 'selected' : '' }}>
+                        {{ $department->name }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
     </div>
+
+    <script>
+        function navigateToDepartmentSelection() {
+            const select = document.getElementById('choose-department');
+            const route = select.value;
+
+            if (route) {
+                window.location.href = route;
+                select.parentNode.classList.add('is-loading');
+            }
+        }
+    </script>
 @endif
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Home')
 
 @section('content')
-    <p class="has-text-centered content">Select a Department.</p>
     @component('components.department-navigation')
         @slot('departments', $departments)
     @endcomponent
