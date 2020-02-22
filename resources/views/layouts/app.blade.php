9fbf82362f8b87aa6c41d3aa324659265dd16359
nickfrostus <nickfrost.us@gmail.com>
2020-02-22 11:19:00 -0500

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <title>@yield('title') - {{ config('app.name', 'Open Org Chart') }}</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.0/css/bulma.min.css" integrity="sha256-D9M5yrVDqFlla7nlELDaYZIpXfFWDytQtiV+TaH6F1I=" crossorigin="anonymous"/>
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300,400,500,600,700&display=swap" rel="stylesheet">
