d14740dcbcf614aa0764034274d876f4f905741b
Nick Frost <42304642+nfrostdev@users.noreply.github.com>
2020-03-13 08:17:38 -0400

    move licence information to index

--- public/index.php
+++ public/index.php
@@ -1,6 +1,29 @@
 <?php
 
 /**
+ * Open Org Chart is distributed as a free, open source alternative
+ * to traditional organization chart software. Aimed at solving the
+ * issue of displaying complex hierarchy on mobile devices without
+ * needing to pinch and zoom; as well as simplifying the process
+ * of modifying employee-supervisor relations and department
+ * leadership with ease.
+ * Copyright (C) 2020 Nick Frost
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
